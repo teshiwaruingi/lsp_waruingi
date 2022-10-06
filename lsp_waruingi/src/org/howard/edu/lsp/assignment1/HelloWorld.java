0a90c8c46750c256ec85df1f1fbbf901641b2748
Teshi Waruingi <teshi.waruingi@bison.howard.edu>
2022-10-05 22:31:16 -0400

    First commit

---
+++ lsp_waruingi/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,18 @@
+package org.howard.edu.lsp.assignment1;
+
+/**
+ * 
+ * Teshi Waruingi 
+ * 
+ * 
+ * 
+ */
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
